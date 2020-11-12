0c4c428c7dcc84bdd147e768e259ee7313a2a997
Thomas Torrente <thomastorrente@imac-de-thomas.home>
2020-11-12 14:46:34 +0100

    9push

--- src/components/OnePost.js
+++ src/components/OnePost.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
-const OnePost = ({ data }) => {
-  return (
+const OnePost = () => {
+  const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState({});
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      `https://lereacteur-vinted-api.herokuapp.com/offers/${id}`
+    );
+    setData(response.data);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    // Permet de n'afficher qu'une seule fois dans la condition
+    fetchData();
+  }, [id]);
+  return isLoading ? (
+    <div>En cours de chargemment</div>
+  ) : (
     <>
-      {data.offers.map((offre, index) => {
-        return (
-          <>
-            <div>
-              <img src={offre.product_image.secure_url} alt="" />
-            </div>
-
-            {offre.product_details.map()}
-          </>
-        );
-      })}
+      <div>
+        <p>{data.product_name}</p>
+      </div>
     </>
   );
 };
